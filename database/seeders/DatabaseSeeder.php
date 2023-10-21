b933f9b6e132cc5a38222518ff237e5fd69a068d
Rashwan <Ahmed.Rashwan2014@yahoo.com>
2023-10-22 00:27:35 +0300

    Create factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\FuelEntry;
+use App\Models\InsurancePayment;
+use App\Models\Service;
+use App\Models\Vehicle;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +16,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Vehicle::factory(10)->create();
+        FuelEntry::factory(10)->create();
+        Service::factory(10)->create();
+        InsurancePayment::factory(10)->create();
     }
 }
